5078ae32119c6ccb2aa177b3e50f95df9d55afb4
C. Leow <chienleow1@gmail.com>
2020-04-27 14:12:20 +0000

    Automatically backed up by Learn

--- ecommerce-cli/lib/ecommerce/cli.rb
+++
@@ -1,8 +0,0 @@
-require "ecommerce/cli/version"
-
-module Ecommerce
-  module Cli
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- ecommerce-cli/lib/ecommerce/cli/version.rb
+++
@@ -1,5 +0,0 @@
-module Ecommerce
-  module Cli
-    VERSION = "0.1.0"
-  end
-end
